dd4854ffe4f343ffc59807a63ffad83a72f8b465
edita <edignot@gmail.com>
2024-01-06 16:38:43 -0700

    fetching dummy users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Link from 'next/link'
 
-const UsersPage = () => {
+interface User {
+  id: number,
+  name: string
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  const users: User[] = await res.json()
+
   return (
-    <div>
-      Users
-    </div>
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => 
+            <li key={user.id}>{user.name}</li>
+          )}
+      </ul>
+      <Link href="/">Back Home</Link>
+    </>
   )
 }
 
